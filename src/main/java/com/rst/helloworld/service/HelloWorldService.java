f0e6e81b583b3369cfc133c80e5fd5218815240b
Ghana710 <130124833+Ghana710@users.noreply.github.com>
2023-12-28 21:03:25 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello  ,GHANA CHANDRA BHARATH is good boy,Bangalore";
+			return "Hello  ,God is great";
 		}else{
 			return "Hello " + name;
 		}
